254df899be01a08a370e93fde593ee55246b35da
dlernatovich <dmytro.lernatovych@zasio.com>
2021-01-12 16:53:24 +0200

    Added extensions

---
+++ Sources/ArtliteBaseAppleLibrary/Extensions/NSObject+Additional.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by dlernatovich on 12.01.2021.
+//
+
+import Foundation
+#if os(iOS)
+import UIKit
+
+/// Extension for the {@link NSObject}
+@objc public extension NSObject {
+    
+    /// Method which provide the getting of the top most visible controller
+    /// - Returns: instance of the {@link UIViewController}
+    @objc func getVisibleController() -> UIViewController? {
+        return UIApplication.shared.keyWindow?.rootViewController?.getVisibleController()
+    }
+    
+}
+#endif
--- Tests/ArtliteBaseAppleLibraryTests/ArtliteBaseAppleLibraryTests.swift
+++ Tests/ArtliteBaseAppleLibraryTests/ArtliteBaseAppleLibraryTests.swift
@@ -6,7 +6,7 @@ final class ArtliteBaseAppleLibraryTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ArtliteBaseAppleLibrary().text, "Hello, World!")
+//        XCTAssertEqual(ArtliteBaseAppleLibrary().text, "Hello, World!")
     }
 
     static var allTests = [
