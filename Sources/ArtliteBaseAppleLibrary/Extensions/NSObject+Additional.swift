ddac355911b191d6a77c734801f49c67c76ff71f
dlernatovich <dmytro.lernatovych@zasio.com>
2021-01-12 17:33:36 +0200

    Added show dialog method from any place

--- Sources/ArtliteBaseAppleLibrary/Extensions/NSObject+Additional.swift
+++ Sources/ArtliteBaseAppleLibrary/Extensions/NSObject+Additional.swift
@@ -21,4 +21,33 @@ import UIKit
     }
     
 }
+
+/// Extension for the {@link NSObject}
+@objc public extension NSObject {
+    
+    /// Method which provide the show alert dialog from any place.
+    /// - Parameters:
+    ///   - style: dialog style
+    ///   - title: title value
+    ///   - message: message value
+    ///   - closeButton: close buttin message
+    ///   - closeButtonStyle: close button style (default is UIAlertAction.Style.default)
+    ///   - customization: customization callback
+    @objc func showDialog(
+        style: UIAlertController.Style = .alert,
+        title: String? = nil,
+        message: String,
+        closeButton: String,
+        closeButtonStyle: UIAlertAction.Style = .default,
+        customization: ((UIAlertController) -> Void)? = nil
+    ) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        alert.addAction(UIAlertAction(title: NSLocalizedString(closeButton, comment: ""), style: closeButtonStyle, handler: { (action) in
+            
+        }))
+        customization?(alert)
+        self.afVisibleController?.present(alert, animated: true);
+    }
+    
+}
 #endif
