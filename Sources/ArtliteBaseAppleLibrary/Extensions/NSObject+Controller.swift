eeed79fef668f7f9bc3f6298f8e1285fcf238b37
dlernatovich <dmytro.lernatovych@zasio.com>
2021-01-12 18:08:00 +0200

    Extended dialog functional possibility

---
+++ Sources/ArtliteBaseAppleLibrary/Extensions/NSObject+Controller.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by dlernatovich on 12.01.2021.
+//
+
+import Foundation
+#if os(iOS)
+import UIKit
+
+/// Extension for the {@link NSObject}
+@objc public extension NSObject {
+    
+    /// Method which provide the getting of the top most visible controller
+    /// - Returns: instance of the {@link UIViewController}
+    @objc var afVisibleController: UIViewController? {
+        get {
+            return UIApplication.shared.keyWindow?.rootViewController?.getVisibleController()
+        }
+    }
+    
+}
+#endif
---
+++ Sources/ArtliteBaseAppleLibrary/Extensions/NSObject+Dialog.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by dlernatovich on 12.01.2021.
+//
+
+import Foundation
