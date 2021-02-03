303b6064f80beca0c376a353e3790e2bbd900a21
dlernatovich <dmytro.lernatovych@zasio.com>
2021-02-03 17:09:35 +0200

    Small fixes

--- Sources/ArtliteBaseAppleLibrary/Extensions/UIColor/UIColor+Hex.swift
+++ Sources/ArtliteBaseAppleLibrary/Extensions/UIColor/UIColor+Hex.swift
@@ -19,7 +19,17 @@ public extension UIColor {
         if hex.hasPrefix("#") {
             let start = hex.index(hex.startIndex, offsetBy: 1)
             let hexColor = String(hex[start...])
-            if hexColor.count == 8 {
+            if hexColor.count == 6 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    self.init(red: r, green: g, blue: b, alpha: 1.0)
+                    return
+                }
+            } else if hexColor.count == 8 {
                 let scanner = Scanner(string: hexColor)
                 var hexNumber: UInt64 = 0
                 if scanner.scanHexInt64(&hexNumber) {
