56c99731c8b1c28b0a9523ea8d3254acbd418d6b
dlernatovich <dmytro.lernatovych@zasio.com>
2021-02-03 14:45:10 +0200

    Added states

--- Sources/ArtliteBaseAppleLibrary/Extensions/UIControl/UIControl+Additional.swift
+++ Sources/ArtliteBaseAppleLibrary/Extensions/UIControl/UIControl+Additional.swift
@@ -15,6 +15,15 @@ public extension UIControl.State {
     /// Array of the all states
     static var allStates: [UIControl.State] { [.normal, .highlighted, .disabled, .selected, .focused, .application, .reserved] }
     
+    /// Array of the normal states
+    static var normalStates: [UIControl.State] { [.normal] }
+    
+    /// Array of the disabled states
+    static var disabledStates: [UIControl.State] { [.disabled] }
+    
+    /// Array of the pressed states
+    static var pressedStates: [UIControl.State] { [.highlighted, .selected, .focused] }
+    
 }
 
 #endif
