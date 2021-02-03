b6a8c98568948bb2e333512c7b06314e704f4907
dlernatovich <dmytro.lernatovych@zasio.com>
2021-02-03 17:18:30 +0200

    Fixes

--- Sources/ArtliteBaseAppleLibrary/Extensions/UIColor/UIColor+Hex.swift
+++ Sources/ArtliteBaseAppleLibrary/Extensions/UIColor/UIColor+Hex.swift
@@ -21,23 +21,12 @@ public extension UIColor {
             let hexColor = String(hex[start...])
             if hexColor.count == 6 {
                 let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                var hexNumber: UInt32 = 0
+                if scanner.scanHexInt32(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 16) / 255.0
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 8) / 255.0
+                    b = CGFloat((hexNumber & 0x0000ff00)) / 255.0
                     self.init(red: r, green: g, blue: b, alpha: 1.0)
-                    return
-                }
-            } else if hexColor.count == 8 {
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
-                    self.init(red: r, green: g, blue: b, alpha: a)
                     return
                 }
             }
